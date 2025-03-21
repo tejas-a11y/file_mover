9acf910f9d4fcfbddbf26468f4231b7b0520fd66
tejas-a11y <tejasshrivastava4@gmail.com>
2025-03-21 15:56:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+#Importing watchdog and os module for handling file system events and accessing list of folder directory.
+
 from watchdog.observers import Observer
 import time
 from watchdog.events import FileSystemEventHandler
 import os
 
+#Specifying file extentions under each category of files.
 pictures = ['.png','.jpg','.jpeg','.gif','.svg']
 videos = ['.mp4','.mov','.avi','.wmv','.flv']
+
+'''Creating a MyHandler class as per watchdog docs page and under on_modified method (as to be written under docs to be used), specifying a for loop for each event to get 
+extention on file by os.path.splitext() method and then adding conditional logic changing the destination as per file extention'''
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
@@ -19,6 +25,7 @@ class MyHandler(FileSystemEventHandler):
                 new_destination = folder_destination + '/documents/' + filename
             os.rename(src, new_destination)
 
+#Specifying folder to track and destination in which subfolders are there for file types.
 
 folder_to_track = "C:/Users/Tejas/Desktop/myFolder"
 folder_destination = "C:/Users/Tejas/Desktop/newFolder"
